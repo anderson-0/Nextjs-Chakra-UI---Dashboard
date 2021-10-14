5169c6265e6811f6d5d8ffa42c90e73a247bf7e6
Anderson <anderson.consultant.it@outlook.com>
2021-10-14 18:42:48 -0400

    feature: created Input component and refactored sign in form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,13 @@
-import { Button, Flex, FormControl, FormLabel, Input, Stack } from '@chakra-ui/react'
+import { Button, Flex, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input';
 
-export default function Home() {
+export default function SignIn() {
   return (
     <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
       <Flex as="form" w="100%" maxW={360} bg="gray.800" padding="8" borderRadius={8} flexDirection="column">
         <Stack spacing={4}>
-          <FormControl>
-            <FormLabel htmlFor="email">E-mail</FormLabel>
-            <Input 
-              name="email" 
-              id="email"
-              type="email"
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size="lg"
-            />
-          </FormControl>
-
-            <FormControl>
-            <FormLabel htmlFor="password">Senha</FormLabel>
-            <Input
-              name="password"
-              id="password"
-              type="password"
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size="lg"
-            />
-          </FormControl>
+          <Input type="email" name="email" label="E-mail"/>
+          <Input type="password" name="password" label="Senha"/>
         
           <Button type="submit" marginTop="6" colorScheme="pink" size="lg">Entrar</Button>
         </Stack>
