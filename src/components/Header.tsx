c1b966ffe6f81efc1e32c6c805c5512e239f5ee1
Anderson <anderson.consultant.it@outlook.com>
2021-10-18 00:26:44 -0400

    featured: added missing styling to header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,52 +1,78 @@
-import { Flex, Input as ChakraInput, Text, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from 'react-icons/ri';
+import { Flex, Input as ChakraInput, Text, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
 
 export function Header() {
-    return (
-        <Flex 
-            as="header"
-            width="100%"
-            maxWidth={1480}
-            height="20"
-            marginX="auto"
-            marginTop="4"
-            paddingX="6"
+  return (
+      <Flex 
+        as="header"
+        width="100%"
+        maxWidth={1480}
+        height="20"
+        marginX="auto"
+        marginTop="4"
+        paddingX="6"
+        align="center"
+      >
+          <Text 
+            fontSize="3xl" 
+            fontWeight="bold" 
+            letterSpacing="tight"
+            width="64"
+          >
+            DashGo
+            <Text as="span" color="pink.500" marginLeft="1">.</Text>
+          </Text>
+
+          <Flex
+            as="label"
+            flex="1"
+            paddingY="4"
+            paddingX="8"
+            marginLeft="6"
+            maxWidth={400}
+            alignSelf="center"
+            color="gray.200"
+            position="relative"
+            bg="gray.800"
+            borderRadius="full"
+          >
+            <ChakraInput
+              color="gray.50"
+              variant="unstyled"
+              placeholder="Buscar na plataforma"
+              _placeholder={{color: 'gray.400'}}
+              paddingX="4"
+              marginRight="4"
+            />
+            <Icon as={RiSearchLine} fontSize="20"/>
+          </Flex>
+      
+          <Flex
             align="center"
-        >
-            <Text 
-                fontSize="3xl" 
-                fontWeight="bold" 
-                letterSpacing="tight"
-                width="64"
-            >
-                DashGo
-                <Text as="span" color="pink.500" marginLeft="1">.</Text>
-            </Text>
-
-            <Flex
-                as="label"
-                flex="1"
-                paddingY="4"
-                paddingX="8"
-                marginLeft="6"
-                maxWidth={400}
-                alignSelf="center"
-                color="gray.200"
-                position="relative"
-                bg="gray.800"
-                borderRadius="full"
-            >
-                <ChakraInput
-                    color="gray.50"
-                    variant="unstyled"
-                    placeholder="Buscar na plataforma"
-                    _placeholder={{color: 'gray.400'}}
-                    paddingX="4"
-                    marginRight="4"
-                />
-                <Icon as={RiSearchLine} fontSize="20"/>
-
-            </Flex>
-        </Flex>
-    );
+            marginLeft="auto"
+          >
+              <HStack 
+                spacing="8"
+                marginX="8"
+                paddingRight="8"
+                paddingY="1"
+                color="gray.300"
+                borderRightWidth={1}
+                borderColor="gray.700"
+              >
+                <Icon as={RiNotificationLine} fontSize="20"/>
+                <Icon as={RiUserAddLine} fontSize="20"/>
+              </HStack>
+              <Flex
+                align="center"
+              >
+                <Box marginRight="4" textAlign="right">
+                  <Text>Diego Fernandes</Text>
+                  <Text color="gray.300">diego.schell@gmail.com</Text>
+                </Box>
+                <Avatar size="md" name="Diego Fernandes" src="https://github.com/diego3g.png" />
+              </Flex>
+          </Flex>
+      </Flex>
+  );
 }
